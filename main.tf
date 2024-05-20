d13857bd2b490b387a16d53760f01b15aec42466
Gad ndayishimiye <nday.gady@gmail.com>
2024-05-20 13:19:36 -0700

    update on version

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
- region = "us-east-1" 
+ region = var.REGION 
 }
 resource "aws_lightsail_instance" "server1" {
   name = "dev-server1"
   blueprint_id = "ubuntu_18_04"
-  bundle_id = "medium_1_0"
+  bundle_id = var.INSTANCE_TYPE
   availability_zone = "us-east-1"
   user_data = <<-EOF
   #!/bin/bash
@@ -14,4 +14,7 @@ resource "aws_lightsail_instance" "server1" {
     sudo systemctl enable apache2
     echo '<h1>This is deployed by Serge </h1>' | sudo tee /var/www/html/index.html
     EOF
+    tags = {
+      name= var.ENVIRONMENT
+    }
 }
